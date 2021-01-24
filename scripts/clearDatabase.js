04cf8d669cde7cdadb86e3906c4a9d70ccb28c3a
Nicholas Wong <sword9200@gmail.com>
2021-01-24 11:19:56 +0100

    Add more logging to Database Initialization

--- scripts/clearDatabase.js
+++ scripts/clearDatabase.js
@@ -2,9 +2,13 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URL)
     .then(() => {
         mongoose.connection.db.dropCollection('User')
-            .then(process.exit(0));
+            .then(() => {
+                console.log("Database Initialization successful!")
+                process.exit(0);
+            });
     })
     .catch(err => {
+        console.log("Database Initialization failed! :(")
         console.error(err);
         process.exit(1);
     });
