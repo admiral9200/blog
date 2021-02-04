66806adaf9bb86e2556bdbe33cdd70b26f7af517
Nicholas Wong <sword9200@gmail.com>
2021-02-04 23:22:28 +0100

    Add account.js route

---
+++ api/account.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const { body, validationResult } = require('express-validator');
+const User = require('../models/User');
+
+router.get('', (req, res) => {
+    User.findById(req.session.userId)
+    .then(user => {
+        let retobj = {username: user.username, email: user.email, registered: user.registered, level: user.level}
+        res.json({success: true, data: retobj});
+    })
+    .catch(err => {
+        let error = new Error("Something went wrong..");
+        errror.status = 500;
+        return next(error);
+    })
+});
+
+module.exports = router;
