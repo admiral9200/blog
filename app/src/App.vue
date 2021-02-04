e58a93380f0d04141a8daaacfd3ac0b10790507d
Nicholas Wong <sword9200@gmail.com>
2021-02-04 23:22:01 +0100

    Add notifications and animations

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,29 +1,77 @@
 <template>
   <div id="app">
     <div class="c">
-      <account/>
-      <h1 class="center"><router-link to="/">{{ $route.name }}</router-link></h1>
-      <router-view/>
+      <account />
+      <h1 class="center">
+        <router-link to="/">{{ $route.name }}</router-link>
+      </h1>
+      <transition-group mode="out-in" name="fade">
+        <notification
+          v-for="notification in $globals.activeNotifications"
+          :key="notification.id"
+          :type="notification.status"
+        >
+          {{ notification.message }}
+        </notification>
+      </transition-group>
+      <transition mode="out-in" name="fade">
+        <router-view />
+      </transition>
     </div>
-    <br><br><br>
+    <br /><br /><br />
     <footer class="full footer">
-      (c) 2021 Julian Blazek. <label for="toggle-theme">Change Theme:</label> <button class="b" id="toggle-theme" @click="changeTheme">🕑</button><br>
-      View the sourcecode on <a href="https://github.com/nailujx86/simple-blog" target="_blank" rel="noreferrer">Github</a>.
+      (c) 2021 Julian Blazek. <label for="toggle-theme">Change Theme:</label>
+      <button class="b" id="toggle-theme" @click="changeTheme">🕑</button><br />
+      View the sourcecode on
+      <a
+        href="https://github.com/nailujx86/simple-blog"
+        target="_blank"
+        rel="noreferrer"
+        >Github</a
+      >.
     </footer>
   </div>
 </template>
 
 <script>
-import {toggleTheme} from './assets/darkmode';
-import Account from '@/components/Account.vue';
+import { toggleTheme } from "./assets/darkmode";
+import Account from "@/components/Account.vue";
+import Notification from "@/components/Notification.vue";
+
 export default {
   methods: {
     changeTheme() {
       toggleTheme();
-    }
+    },
   },
   components: {
-    Account
-  }
-}
+    Account,
+    Notification,
+  },
+};
 </script>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 0.8s;
+  transition-property: opacity;
+  transition-timing-function: ease;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0
+}
+
+.notification-enter-active, .notification-leave-active {
+  transition: all .2s;
+}
+.notification-enter, .notification-leave-to{
+  opacity: 0;
+}
+.notification-enter-active {
+  transition-delay: .2s;
+}
+
+</style>
--- app/src/store.js
+++ app/src/store.js
@@ -1,17 +1,33 @@
 import Vue from 'vue';
 export var globalStore = new Vue({
+    name: 'store',
     data() {
         return {
-            loggedIn: false
+            loggedIn: false,
+            notificationStorage: []
         }
     },
     created() {
         this.loggedIn = JSON.parse(window.localStorage.getItem('loggedIn'));
     },
+    computed: {
+        activeNotifications() {
+            return this.notificationStorage.filter(notification => notification.active);
+        }
+    },
     methods: {
         setLoggedIn(state) {
             window.localStorage.setItem('loggedIn', JSON.stringify(state))
             this.loggedIn = state;
+        },
+        addNotification(message, status, timeout) {
+            var notificationObj = { message, status, active: true, id: Math.random() };
+            this.notificationStorage.push(notificationObj);
+            if (timeout) {
+                setTimeout(() => {
+                    notificationObj.active = false;
+                }, timeout);
+            }
         }
     },
 });
