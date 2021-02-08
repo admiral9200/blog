32aa81bd733936329cb5144ea0c05654e46ce8f7
Nicholas Wong <sword9200@gmail.com>
2021-02-08 21:08:45 +0100

    Add logic to compose route

--- app/src/views/Compose.vue
+++ app/src/views/Compose.vue
@@ -3,31 +3,39 @@
         <div class="i">
             <div class="card gimme_space">
                 <div class="c">
-                    <form>
+                    <form @submit.prevent="post">
+                        <label for="title">Title</label>
                         <input
                             class="full"
                             type="text"
                             v-model="title"
+                            id="title"
                             placeholder="Title"
                             required
                         /><br />
+                        <label for="imageurl">Image URL</label>
                         <input
                             class="full"
                             type="url"
                             v-model="imageurl"
+                            id="imageurl"
                             placeholder="Image URL"
                         /><br />
+                        <label for="content">Content</label>
                         <textarea
                             rows="10"
                             class="full"
                             v-model="content"
+                            id="content"
                             placeholder="Content (markdown)"
                             required
                         ></textarea>
+                        <label for="summary">Summary</label>
                         <textarea
                             rows="5"
                             class="full"
                             v-model="summary"
+                            id="summary"
                             placeholder="Summary shown on the front page (plaintext)"
                             required
                         ></textarea>
@@ -44,6 +52,8 @@
 
 <script>
 import Post from "../components/Post.vue";
+import router from "../router";
+
 export default {
     data() {
         return {
@@ -66,6 +76,27 @@ export default {
     },
     components: {
         Post,
+    },
+    methods: {
+        post: function () {
+            this.$http
+                .post("/api/post", this.$data, { withCredentials: true })
+                .then((res) => {
+                    this.$globals.addNotification(
+                        "Post created!",
+                        "success",
+                        5000
+                    );
+                    router.push(`/post/${res.data.id}`);
+                })
+                .catch((err) => {
+                    this.$globals.addNotification(
+                        `There was an error creating this post! Error: ${err.response.data.message}`,
+                        "error",
+                        5000
+                    );
+                });
+        },
     },
 };
 </script>
